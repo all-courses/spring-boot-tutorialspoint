bc8c9a9370765910cc10ef9706f1003aff640822
Vijay R. Gawade <VijayGawade96@outlook.com>
2021-11-05 16:29:42 +0530

    Logging using slf4j logger

--- code/TutorialsPoint/src/main/java/com/tp/tutorialspoint/TutorialsPointApplication.java
+++ code/TutorialsPoint/src/main/java/com/tp/tutorialspoint/TutorialsPointApplication.java
@@ -1,5 +1,7 @@
 package com.tp.tutorialspoint;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -8,12 +10,17 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TutorialsPointApplication extends SpringBootServletInitializer implements ApplicationRunner{
+public class TutorialsPointApplication extends SpringBootServletInitializer implements ApplicationRunner {
+	private static final Logger logger = LoggerFactory.getLogger(TutorialsPointApplication.class);
 
 	public static void main(String[] args) {
+		logger.info("this is a info message");
+		logger.warn("this is a warn message");
+		logger.error("this is a error message");
+
 		SpringApplication.run(TutorialsPointApplication.class, args);
 	}
-	
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(TutorialsPointApplication.class);
--- code/TutorialsPoint/src/main/java/com/tp/tutorialspoint/controller/HomeController.java
+++ code/TutorialsPoint/src/main/java/com/tp/tutorialspoint/controller/HomeController.java
@@ -1,14 +1,20 @@
 package com.tp.tutorialspoint.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.tp.tutorialspoint.TutorialsPointApplication;
+
 @Controller
 public class HomeController {
 
+	private static final Logger logger = LoggerFactory.getLogger(TutorialsPointApplication.class);
+	
 	@Value("${spring.application.name}")
 	private String appName;
 	
@@ -21,6 +27,8 @@ public class HomeController {
 	@RequestMapping(method = RequestMethod.GET, path = "/" )
 	@ResponseBody
 	public String welcomePage() {
+		logger.info("Welcome page controller called ...");
+		
 		return "Welcome to Spring Boot !!!</br> By "+developerBy+" @ "+appName+"-"+developeYear;
 	}
 }
