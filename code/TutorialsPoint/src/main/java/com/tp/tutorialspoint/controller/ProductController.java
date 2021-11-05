8f0fbdc48cbd5c38b46cb61ae8fc57aa5bdf3ba5
Vijay R. Gawade <VijayGawade96@outlook.com>
2021-11-05 20:13:29 +0530

    File Handling- Upload & Download image

--- code/TutorialsPoint/src/main/java/com/tp/tutorialspoint/controller/ProductController.java
+++ code/TutorialsPoint/src/main/java/com/tp/tutorialspoint/controller/ProductController.java
@@ -1,9 +1,16 @@
 package com.tp.tutorialspoint.controller;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,6 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import org.apache.commons.io.FilenameUtils;
 
 import com.tp.tutorialspoint.exception.ProductNotfoundException;
 import com.tp.tutorialspoint.model.Product;
@@ -45,9 +55,9 @@ public class ProductController {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
 	public ResponseEntity<Object> updateProduct(@PathVariable("id") int id, @RequestBody Product product) {
-		if(!productRepo.containsKey(id))
+		if (!productRepo.containsKey(id))
 			throw new ProductNotfoundException();
-		
+
 		productRepo.remove(id);
 		product.setId(id);
 		productRepo.put(id, product);
@@ -56,11 +66,44 @@ public class ProductController {
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
 	public ResponseEntity<Object> delete(@PathVariable("id") int id) {
-		if(!productRepo.containsKey(id))
+		if (!productRepo.containsKey(id))
 			throw new ProductNotfoundException();
-		
+
 		productRepo.remove(id);
 		return new ResponseEntity<>("Product is deleted successsfully", HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/upload/{id}", method = RequestMethod.POST, consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+	public String fileUpload(@PathVariable("id") int id, @RequestParam("file") MultipartFile file) throws IOException {
+		File convertFile = new File("./TutorialsPoint/Product_Images/"
+				+ String.format("Product_%d.%s", id, FilenameUtils.getExtension(file.getOriginalFilename())));
+		convertFile.createNewFile();
+		FileOutputStream fout = new FileOutputStream(convertFile);
+		fout.write(file.getBytes());
+		fout.close();
+		return "File is upload successfully";
+	}
+
+	@RequestMapping(value = "/download/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Object> downloadFile(@PathVariable("id") int id) throws IOException {
+		String filename = String.format("./TutorialsPoint/Product_Images/Product_%d.jpg", id);
+		File file = new File(filename);
+		InputStreamResource resource = new InputStreamResource(new FileInputStream(file));
+		HttpHeaders headers = new HttpHeaders();
+
+		headers.add("Content-Disposition", String.format("attachment; filename=\"%s\"", file.getName()));
+		headers.add("Cache-Control", "no-cache, no-store, must-revalidate");
+		headers.add("Pragma", "no-cache");
+		headers.add("Expires", "0");
+
+		ResponseEntity<Object> responseEntity = ResponseEntity.ok()
+				.headers(headers)
+				.contentLength(file.length())
+				.contentType(MediaType.APPLICATION_OCTET_STREAM)
+//				.contentType(MediaType.parseMediaType("image/jpeg"))
+				.body(resource);
+
+		return responseEntity;
 	}
 
 }
