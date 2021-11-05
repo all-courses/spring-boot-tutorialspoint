357177362bce2e199da6f22a176fd752bffa2376
Vijay R. Gawade <VijayGawade96@outlook.com>
2021-11-05 15:35:29 +0530

    Add Application Runner

--- code/TutorialsPoint/src/main/java/com/tp/tutorialspoint/TutorialsPointApplication.java
+++ code/TutorialsPoint/src/main/java/com/tp/tutorialspoint/TutorialsPointApplication.java
@@ -1,12 +1,14 @@
 package com.tp.tutorialspoint;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TutorialsPointApplication extends SpringBootServletInitializer {
+public class TutorialsPointApplication extends SpringBootServletInitializer implements ApplicationRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TutorialsPointApplication.class, args);
@@ -17,4 +19,8 @@ public class TutorialsPointApplication extends SpringBootServletInitializer {
 		return application.sources(TutorialsPointApplication.class);
 	}
 
+	@Override
+	public void run(ApplicationArguments arg0) throws Exception {
+		System.out.println("Execute the code after the Spring Boot application is started");
+	}
 }
