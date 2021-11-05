a7d2890fe78a1c146c5d6fb4b3a276f60f6303e6
Vijay R. Gawade <VijayGawade96@outlook.com>
2021-11-05 15:41:19 +0530

    @Value annotation is used to read the environment or application property value in Java code

--- code/TutorialsPoint/src/main/java/com/tp/tutorialspoint/controller/HomeController.java
+++ code/TutorialsPoint/src/main/java/com/tp/tutorialspoint/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.tp.tutorialspoint.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +9,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
+	@Value("${spring.application.name}")
+	private String appName;
+	
+	@Value("${developerBy}")
+	private String developerBy;
+	
 	@RequestMapping(method = RequestMethod.GET, path = "/" )
 	@ResponseBody
 	public String welcomePage() {
-		return "Welcome to Spring Boot !!!";
+		return "Welcome to Spring Boot !!!</br> By "+developerBy+" @ "+appName;
 	}
 }
